aa22bf7f45b677886242bd5a5f7d4c95da2b06df
BurgerBobby <ethan.haynes@ontariotechu.net>
2025-02-06 17:37:42 -0500

    quick push

---
+++ Year4/Intermediate/Build/Win64/x64/Year4/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34120
+#include "C:/Users/hayne/OneDrive/Documents/Unreal Projects/Year4git/Year4/Intermediate/Build/Win64/x64/Year4/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h"
---
+++ Year4/Intermediate/Build/Win64/x64/Year4/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/hayne/OneDrive/Documents/Unreal Projects/Year4git/Year4/Intermediate/Build/Win64/x64/Year4/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
